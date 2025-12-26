370bdb55e384d79cb182081c28659be643f19322
narzulloevh1308-eng <narzulloev.h1308@gmail.com>
2025-12-26 19:24:50 +0500

    Add main.py with basic functionality and input

---
+++ LR_01/main.py
@@ -0,0 +1,12 @@
+# Проверка работоспособности Visual Studio Code
+
+print("Visual Studio Code работает корректно")
+
+a = 5
+b = 3
+c = a + b
+
+print("Сумма чисел:", c)
+
+name = input("Введите имя: ")
+print("Здравствуйте,", name)
